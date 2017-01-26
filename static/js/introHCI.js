6f48b9a19080650a04ea67787539c3b1ee96d293
tusharkoul <tusharkoul@gmail.com>
2017-01-26 10:58:29 -0800

    lab 3 changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,16 +3,34 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$('.jumbotron h1').text("Experience Chan! It's not a lot of questions.Too many questions is the Chan disease.The best way is just to observe the noise of the world.The answer to your questions?Ask your own heart."
+		);
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").toggleClass("active");
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    $("a.thumbnail").click(projectClick);
+}
+
+
+function projectClick(e) {
+    // prevent the page from reloading 
+    e.preventDefault();
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+        $(containingProject).append("<div class='project-description'><p id='descripition-p-toggle'>Description of the project.</p></div>");
+    } else {
+        // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+        $(containingProject).find("#descripition-p-toggle").hide();
+    }
+
 }
